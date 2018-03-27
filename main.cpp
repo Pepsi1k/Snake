5775f5429c76f7be34947964307e1e279c205eaf
Pepsi1k <megasharff@gmail.com>
2018-03-27 22:13:04 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <clocale>
+#include <Windows.h>
+
+using namespace std;
+
+int main(){
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "рфшен ленгвич";
+    system("pause");
+    return 0;
+}
